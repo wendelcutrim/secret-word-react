16d29a723af01bc0f470900aa51eaa8feec575c6
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-09-03 14:45:17 -0300

    Criado o jsx e estilização do componente game

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -2,9 +2,33 @@ import './style.css';
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar Jogo</button>
+    <div className='game'>
+      <p className='points'>
+        <span>Pontuação: 000</span>
+      </p>
+
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className='tip'>
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="word-container">
+        <span className='letter'>A</span>
+        <span className='blank-square'></span>
+      </div>
+
+      <div className="letter-container">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter"  maxLength="1" required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+
+      <div className="wrong-letters-container">
+        <p>Letras já utilizadas</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   )
 }
