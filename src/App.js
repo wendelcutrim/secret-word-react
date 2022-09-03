2629acc3f0516bbc2133fd4ae0da742196e0d26f
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-09-03 13:48:37 -0300

    Criado as funções para escolher as categorias e palavra de forma aleatória de acordo com a categoria escolhida

--- src/App.js
+++ src/App.js
@@ -21,8 +21,40 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState("");
+
+  const pickWordAndCategory = () => {
+    //Obter uma categoria aleatória
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category);
+
+    //Obter uma palavra aletória da categoria.
+    const word = words[category][Math.floor(Math.random() * Object.keys(category).length)];
+    console.log(word);
+
+    return {word, category};
+  }
+
   //Exibir o componente StartScreen e iniciar o jogo;
   const startGame = () => {
+
+    //Pegar a palavra e categoria
+    const {word, category} = pickWordAndCategory();
+
+    //Criando o array de letras;
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map(letter => letter.toLowerCase());
+
+    console.log(wordLetters);
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   }
 
