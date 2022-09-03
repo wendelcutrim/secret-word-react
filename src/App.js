b100502cb9733a9afb359eb0fc00360dba7b6237
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-09-03 12:55:54 -0300

    Criado componente da tela inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import StartScreen from './components/StartScreen';
+
 function App() {
   return (
     <div className="App">
-      <h1>Secret Word</h1>
+      <StartScreen />
     </div>
   );
 }
